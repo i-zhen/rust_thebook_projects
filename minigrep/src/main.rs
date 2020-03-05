cc8ee3a389d123f0255a2f911137fec448565af7
Z.Y <i-zhen@users.noreply.github.com>
2020-03-05 23:43:19 +0800

    update

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else( |error| {
-        println!("shit! {}", error);
+        eprintln!("shit! {}", error);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("motherfucker!, cannot open file {}", e);
+        eprintln!("motherfucker!, cannot open file {}", e);
         process::exit(1);
     }
 }
